66adb7de622434a83efdfed6572d312b32d09c81
vitaliiboiko360 <vukelan@gmail.com>
2026-06-19 22:14:21 +0300

    correct navigation for the whole app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:swim_swim/app/swimer_app.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:swim_swim/app/swimmer_app.dart';
 
 void main() {
-  runApp(const SwimerApp());
+  setUrlStrategy(PathUrlStrategy());
+  runApp(const SwimmerApp());
 }
