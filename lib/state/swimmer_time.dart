5c7154773ae0fc576e321cc1c7365e375445672e
vitaliiboiko360 <vukelan@gmail.com>
2026-06-19 22:08:42 +0300

    add cubit state for swimmer time

---
+++ lib/state/swimmer_time.dart
@@ -0,0 +1,59 @@
+import 'package:bloc/bloc.dart';
+
+const maxDisplayMinutes = 9;
+const maxDisplaySeconds = 59;
+
+class SwimmerTimeState {
+  final int minutes;
+  final int seconds;
+
+  const SwimmerTimeState(this.minutes, this.seconds);
+
+  int getTotalSeconds() => (minutes * 60) + seconds;
+
+  SwimmerTimeState copyWith({int? minutes, int? seconds}) {
+    return SwimmerTimeState(minutes ?? this.minutes, seconds ?? this.seconds);
+  }
+}
+
+class SwimmerTimeCubit extends Cubit<SwimmerTimeState> {
+  SwimmerTimeCubit() : super(SwimmerTimeState(0, 0));
+
+  void updateTotalSeconds(int seconds) {
+    emit(state.copyWith(seconds: seconds));
+  }
+
+  void updateTime(int minutes, int seconds) {
+    emit(state.copyWith(minutes: minutes, seconds: seconds));
+  }
+
+  void incrementMinutes() {
+    if (state.minutes < maxDisplayMinutes) {
+      emit(state.copyWith(minutes: state.minutes + 1, seconds: state.seconds));
+    }
+  }
+
+  void decrementMinutes() {
+    if (state.minutes > 0) {
+      emit(state.copyWith(minutes: state.minutes - 1, seconds: state.seconds));
+    }
+  }
+
+  void incrementSeconds() {
+    if (state.seconds < maxDisplaySeconds) {
+      emit(state.copyWith(minutes: state.minutes, seconds: state.seconds + 1));
+    } else if (state.minutes < maxDisplayMinutes) {
+      emit(state.copyWith(minutes: state.minutes + 1, seconds: 0));
+    }
+  }
+
+  void decrementSeconds() {
+    if (state.seconds > 0) {
+      emit(state.copyWith(minutes: state.minutes, seconds: state.seconds - 1));
+    } else if (state.minutes > 0) {
+      emit(
+        state.copyWith(minutes: state.minutes - 1, seconds: maxDisplaySeconds),
+      );
+    }
+  }
+}
