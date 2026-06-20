1fbbb9736021fde1f5db8de81a1935ea1cee200c
vitaliiboiko360 <vukelan@gmail.com>
2026-06-20 17:18:29 +0300

    add user model

---
+++ lib/model/user_model.dart
@@ -0,0 +1,91 @@
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final String phone;
+  final String website;
+  final Address address;
+  final Company company;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.phone,
+    required this.website,
+    required this.address,
+    required this.company,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      username: json['username'] as String,
+      email: json['email'] as String,
+      phone: json['phone'] as String,
+      website: json['website'] as String,
+      address: Address.fromJson(json['address']),
+      company: Company.fromJson(json['company']),
+    );
+  }
+}
+
+class Company {
+  final String name;
+  final String catchPhrase;
+  final String bs;
+
+  Company({required this.name, required this.catchPhrase, required this.bs});
+
+  factory Company.fromJson(Map<String, dynamic> json) {
+    return Company(
+      name: json['name'] as String,
+      catchPhrase: json['catchPhrase'] as String,
+      bs: json['bs'] as String,
+    );
+  }
+}
+
+class Address {
+  final String street;
+  final String suite;
+  final String city;
+  final String zipcode;
+  final Geo geo;
+
+  Address({
+    required this.street,
+    required this.suite,
+    required this.city,
+    required this.zipcode,
+    required this.geo,
+  });
+
+  factory Address.fromJson(Map<String, dynamic> json) {
+    return Address(
+      street: json['street'] as String,
+      suite: json['suite'] as String,
+      city: json['city'] as String,
+      zipcode: json['zipcode'] as String,
+      geo: Geo.fromJson(json['geo']),
+    );
+  }
+}
+
+class Geo {
+  final String lat;
+  final String lng;
+
+  Geo({required this.lat, required this.lng});
+
+  factory Geo.fromJson(Map<String, dynamic> json) {
+    return Geo(lat: json['lat'] as String, lng: json['lng'] as String);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'lat': lat, 'lng': lng};
+  }
+}
